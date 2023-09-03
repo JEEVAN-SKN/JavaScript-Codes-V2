6e80a13448f7b2d0253d30e57efd0e40661b6a38
JEEVAN-SKN <saisaranamjeevan@gmail.com>
2023-09-03 19:45:07 +0530

    Intro to execution Context

---
+++ 01jsWork.js
@@ -0,0 +1,22 @@
+// everything in js happens inside the execution context
+// it has 2 components 
+// 1)Memory component(variable environment)
+// 2)Code componen(Thread of Execution)
+
+//1)  In memory component all variables and functions are stored in as key-value pairs
+
+// 2) In Code component all the code is executed one line at a time(interpreted)
+
+//   ** Javascript is a synchronous single threaded language*
+// Here single threaded means that only one command is executed at a time
+//  and synchronous means that these single commands are executed in a specific order 
+
+//  Asynchronous parts comes in ajax and other stuff which we will discudd later 
+
+
+
+
+
+
+
+
