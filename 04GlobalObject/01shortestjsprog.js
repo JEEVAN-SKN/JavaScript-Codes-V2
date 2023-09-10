4cc0d2eaf0fe7535ef5d3a4b508a18505b6f8454
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-09-10 07:20:19 +0530

    explanation addded

--- 04GlobalObject/01shortestjsprog.js
+++ 04GlobalObject/01shortestjsprog.js
@@ -1,28 +1,32 @@
 
 
 // the above code lines(literally no code ) is the shortest program in js
-// if we run an empty code in js browser console there is still lot runnin gin background
+// if we run an empty code in js browser console there is still lot running in background
+//these are done by the js engine (v8 in case of chrome)
 
 //Js engine creates an execution context and its respective call stack for every execution (even empty code)
 // js engine always creates a global object ("window" in case of chrome) and a "this" keyword for an execution even if it is empty code 
-// i.e for every EC a global object and a this keyword is created 
-// in that EC this keyword generally points to the Global object ("window")
-// this === window
+// i.e for every EC a "this" keyword is created  and in global scope  "this" keyword points to Global Object  
+// this === window  -- output: true (in browser console)
 
-// this Global object ("window") is a collection of function and methods loaded up default by js
+// this Global object ("window" incase of chrome) is a collection of function and methods loaded up default by js
 
-// Global Scope -- any variables or functions created in js code except inside {} of another function or object etc is under the global scope
+// Global Scope -- any variables or functions created in js code except inside scope{} of another function or object etc is under the global scope
 
 // any varible or function created in global scope is stored under the global  Object and can also be accessed thru the object
 // for ex:-
 
 // try the following code out in browser console
 
-var a = 10;
+var a = 10; // this var a is in global scope 
+
+function alpha() {
+    var b = 16; // var b is not in global scope as it is inside scope of the fucntion alpha 
+}
 
 this === window // will give out true as in global object this keyword points to the global object(window)
 
-console.log(a);
+console.log(a);  //inn case of var b will show error as it is not in global scope 
 
 console.log(this.a);
 
