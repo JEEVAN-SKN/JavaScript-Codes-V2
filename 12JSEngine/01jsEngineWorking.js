0d93b7f2fb04216fe2a74e9bd5c8b1e669602466
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-09-22 07:27:40 +0530

    Javascript Runtime

---
+++ 12JSEngine/01jsEngineWorking.js
@@ -0,0 +1,16 @@
+    // js can be applied almost anywhere, it canbe run in browser, server, smartwatch, lightbulb, robots and many more  
+        //this is possible due to javascript runtime environment 
+                //js runtime is like a container which has all the things to run javascript code
+                    // js Runtime basically contains a)Js Engine(call stack)
+                                                  // b) WebAPI
+                                                  // c)event loop
+                                                  // d) callback queue 
+                                                  // e) microtask queue
+
+            //all javascript execution in browser happens through this runtime environment 
+        //Node js has its own runtime environment which helps to run js in servers
+    //node js runtime and browser runtime has many similarities and dissimilarities 
+
+// Now lets discuus the working of js engine 
+        //there are a lot of js engines out there like chakra(micorsoft edge), Spidermonkey(firefox), V8(chrome, node.js, Deno, v8.net)
+            //most important principle of a js engine is to follow the ECMA script standards 
