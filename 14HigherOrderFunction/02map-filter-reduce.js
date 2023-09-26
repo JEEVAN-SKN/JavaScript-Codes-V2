700e6040f157f9681247b9749cc0eaa2076ee4aa
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-09-26 22:20:52 +0530

    map()

---
+++ 14HigherOrderFunction/02map-filter-reduce.js
@@ -0,0 +1,10 @@
+// map() function is used to transform every element of an array
+    // for eg 
+        const arr = [5,1,3,2,6]
+    
+    //transformation could be anything like the following   
+        //Double - [10,2,6,4,12]
+        //Triple - [15,3,9,6,18]
+        //Binary - ["101","11","10,"110"]
+
+        
