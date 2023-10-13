d10a8698b27c4cdf21b985d303bee5b82d85a6e9
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-10-13 07:31:26 +0530

    asynchronous function

---
+++ 17AsyncAwait/01asyncawait.js
@@ -0,0 +1,14 @@
+//In this session we're gonna discuss about the follwoing questions
+    // What is async?
+    // What is await?
+    // examples,behind working and error handlig in async await
+    // Async await vs Promise.then/.catch
+
+//async is a keyword to create a async function 
+//async function always returns a promise -> that is, in the function definiton we can either create and return a promise
+                                    //or u can return whatever u want(string or number) but async wraps it in a promise and retunrs it 
+
+async function getData(){
+
+}
+
