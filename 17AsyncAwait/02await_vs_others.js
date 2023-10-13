206e363f365ef7a2c768553b48591e75bc0ba710
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-10-14 00:26:16 +0530

    Multiple await in an async function

--- 17AsyncAwait/02await_vs_others.js
+++ 17AsyncAwait/02await_vs_others.js
@@ -1,4 +1,4 @@
-// //we will dicuss here how await keyword inside an async function makes js to wait for completion of asynchronous function 
+// //we will discuss here how await keyword inside an async function makes js to wait for completion of await function 
 //                 // before executing any other thing inside the  async function 
 //             //note that it doesnt stop all other sync function or code of the whole code 
 //                     //it just stops the exectuion of other code inside the async function block {}
@@ -20,12 +20,21 @@ const p = new Promise((resolve,reject)=>{
 
 // async function retrieveData(){
 //     console.log("namaste javascrpt");  //this will be printed first with no delay
-//     const a = await p;      //but as await is used here , js callstack waits till the async operation finishes execution 
+//     const a = await p;      //but as await is used here , is appears that js waits till the await operation finishes execution 
 //     console.log(p);                 //js waits for the promise to get resolved 
 //     console.log(a);
 //     console.log("namaste js"); //this will only be executed after the above three lines of code is executed 
 // }           //therefore js callstack which usually doesnt wait for async functions is made to wait by using await keyword
 // retrieveData()
+            //But in real the callstack is not waiting , bcs if callstack waits then it would be equivalent ot blocking the main thread
+                        // and blocking the main thread is not recommended in js
+            //hence what happens is that callstack when meets an await methof inside an async function , it just suspends the execution of the async function   
+                                            // till the await method is finished execution or promise is resolved 
+                                            // it doesnt stop the main block only that particulare async function from where await came is suspended 
+                                            //remaining code for sure will be executed while the await promise is being resolved 
+                                            //once the the await promise is resolved, then it executes the next command inside the async function     
+
+
     
 //using two await methods in an async function 
 
@@ -33,8 +42,9 @@ async function Data(){
     const val1 = await p   //we may think first await waits for 5 secs executes then 2nd await waits for 5 seconds and executes 
     console.log(val1);         //but both await function execute at same time though execution is done line by line
                                     //line be line i mean print (p) then hi hello ,then (p) again then he hello
-                                            // but all these execute at same time after 5 second timer in js
-    console.log("hi hello");                //al await in async function run parallel
+                                            // but all these await functions timer starts same time hence when first val1 is printed and it goes to next val2
+                                                    //by that time val2 's 5 second timer has also expired and it has been already pushed into the queue
+    console.log("hi hello");                
 
     const val2 = await p 
     console.log(val2);
@@ -43,3 +53,5 @@ async function Data(){
 
 }
 Data()
+
+//Lets see a more complex example for understanding multiple 
