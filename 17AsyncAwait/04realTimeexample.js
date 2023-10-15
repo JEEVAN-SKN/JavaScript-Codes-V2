c9fee9398e4a3a58569fe33f0951d3f4aadb0921
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-10-15 23:01:37 +0530

    Handling fetch() with async await

--- 17AsyncAwait/04realTimeexample.js
+++ 17AsyncAwait/04realTimeexample.js
@@ -17,5 +17,12 @@ const API_URL = "https://api.github.com/users/JEEVAN-SKN";
 
 async function handlePromise(){
 
+  const data = await fetch(API_URL);
+
+  const jsonValue = await data.json();
+
+  console.log(jsonValue);
+
+}
 
 
