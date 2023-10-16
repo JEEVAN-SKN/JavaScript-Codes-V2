196181ca88880a8ed585e57e90a8741b4e41864a
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-10-16 12:58:27 +0530

    json conversion

--- 17AsyncAwait/04realTimeexample.js
+++ 17AsyncAwait/04realTimeexample.js
@@ -3,9 +3,9 @@
 
 //working of fetch()
     // fetch() is basically a promise when it get resolved (or) when we get data from server we get -> Response object -> 
-                //the response obejct has a body which has a readable stream 
+                //the response object has a body which has a readable stream 
                     // readable stream need to be changed into json 
-                        // Response.json() which again is a promise which needs to be resolved 
+                        // Response.json() which again is a promise which is then resolved 
                         // When it get resolved we get the intended data
 
 // Lets use a free api provided by api.github.com
@@ -15,12 +15,15 @@
 //making api call to the url
 const API_URL = "https://api.github.com/users/JEEVAN-SKN";
 
-async function handlePromise(){
+async function handlePromise(){ //as await is called handlePromise is suspended till await function is done resolved or rejected 
 
-  const data = await fetch(API_URL);
+  const data = await fetch(API_URL);   //fetch() retunrs api result which is response object which inturn saved in 'data'
 
-  const jsonValue = await data.json();
-
+        //We can convert object response body which is a readable stream into json - data.json (we can also convert into string or text) 
+            // here we know that this github api gives result as json hence we use 'data.json'
+                //data.json also gives us a promise as return hence we need to use await to resolve it further 
+  const jsonValue = await data.json();  // handlePromise suspended toll data.json is resolved or rejected inwhich result is stored in jsonValue 
+  
   console.log(jsonValue);
 
 }
