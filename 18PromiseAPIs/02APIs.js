ca1a474baae13dfef4c35ae14aa4a97f961be729
S K N JEEVAN <saisaranamjeevan@gmail.com>
2023-11-01 16:20:12 +0530

    Promise.race vs Promise.any

--- 18PromiseAPIs/02APIs.js
+++ 18PromiseAPIs/02APIs.js
@@ -88,3 +88,86 @@
 
 //------------------------------------------------------------------
 
+//Promise.race 
+// const p1 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P1 Success"),3000);
+// })
+
+// const p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P2 Success"),5000);
+// })
+
+// const p3 = new Promise((resolve, reject) => {
+//     setTimeout(() => reject("P3 Fail"),2000);
+// })
+
+// //p3 is settled faster but also rejected  
+//     //hence race gives the first settled promise and ignores all others 
+// Promise.race([p1, p2, p3])
+// .then((res) => {
+//     console.log(res);     //output: P3 Fail
+// })
+// .catch((err) => {  //we do this to catch th error give a defined response or else uncaught error will display in console in browser(we catch the uncaught error)
+//     // console.log(err);     // just prints the error message like a text
+//     console.error(err);  // prints the error message in console like a error (not uncaught but caught error)
+// })
+
+
+//----------------------------------------------
+
+//Promise.any 
+    //gives us first resolved promise 
+        //if al error then aggregation error 
+
+// CASE1: atleast any one is resolved, then gets that  
+
+// const p1 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P1 Success"),3000);
+// })
+
+// const p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P2 Success"),5000);
+// })
+
+// const p3 = new Promise((resolve, reject) => {
+//     setTimeout(() => reject("P3 Fail"),2000);
+// })
+
+// //p3 is settled faster but also rejected  
+//     //hence race gives the first settled promise and ignores all others 
+// Promise.any([p1, p2, p3])
+// .then((res) => {
+//     console.log(res);     //output: P1 Success -- even if p1 is only settled after p3 it is retrieved because latter p3 has failed 
+// })
+// .catch((err) => {  //we do this to catch th error give a defined response or else uncaught error will display in console in browser(we catch the uncaught error)
+//     // console.log(err);     // just prints the error message like a text
+//     console.error(err);  // prints the error message in console like a error (not uncaught but caught error)
+// })
+
+//------------------------------------------------------
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => reject("P1 Fail"),3000);
+})
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => reject("P2 Fail"),5000);
+})
+
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => reject("P3 Fail"),2000);
+})
+
+//p3 is settled faster but also rejected  
+    //hence race gives the first settled promise and ignores all others 
+Promise.any([p1, p2, p3])
+.then((res) => {
+    console.log(res);     //output: [AggregateError: All promises were rejected] {
+                                //     [errors]: [ 'P1 Fail', 'P2 Fail', 'P3 Fail' ]
+                                //   }
+})
+.catch((err) => {  //we do this to catch th error give a defined response or else uncaught error will display in console in browser(we catch the uncaught error)
+    // console.log(err);     // just prints the error message like a text
+    console.error(err);  // prints the error message in console like a error (not uncaught but caught error)
+    // console.log(err.errors); // used in browser if error of individual promises are not listed in aggregate error
+})                                  // in case of node just logging 'err' brings both aggregate error with all the individual erros listed
